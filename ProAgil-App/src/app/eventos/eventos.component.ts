3beca97bed144fea30add65dc735bfb392fbb132
Leonardo <lcoelho@wisesystem.com.br>
2019-05-02 12:25:06 -0300

    Carga inicial do projeto

---
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent implements OnInit {
+  /*
+  eventos: any = [
+    {
+      EventoId: 1,
+      Tema: 'Angular',
+      Local: 'Belo Horizonte'
+    },
+    {
+      EventoId: 2,
+      Tema: '.NET Core',
+      Local: 'São Paulo'
+    },
+    {
+      EventoId: 3,
+      Tema: 'Angular e .NET Core',
+      Local: 'Rio de Janeiro'
+    }
+  ];
+  */
+  _filtroLista: string;
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+  eventosFiltrados: any = [];
+  eventos: any = [];
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;  
+  
+  
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit() {
+    this.getEventos();
+  }
+  
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+
+  }
+
+  alternarImagem(){
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+  
+  getEventos(){
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+    this.eventos = response;
+    console.log(response);
+  }, error => {
+    console.log(error);
+  });
+}
+
+}
---
+++ ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.Repository;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly ProAgilContext _context;
+
+        public ValuesController(ProAgilContext context){
+
+            _context = context;
+
+        }   
+
+        // GET api/values
+        [HttpGet]
+        //public ActionResult<IEnumerable<Evento>> Get()
+        //public IActionResult Get()
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                //var results = _context.Eventos.ToList();
+                var results = await _context.Eventos.ToListAsync();
+                return Ok(results);       
+            }
+            catch (System.Exception)
+            {
+                
+                //return BadRequest(); 
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+
+            
+
+
+            //return new string[] { "value1", "value2", "WiseSystem .NET" };
+            /* 
+            return new Evento[] { 
+                new Evento() { 
+                    EventoId = 1,                    
+                    Tema = "Angular e .NET Core",
+                    Local = "Belo Horizonte",
+                    Lote = "1 Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                } ,
+                new Evento() {                     
+                    EventoId = 2,                    
+                    Tema = "Angular e Suas Novidades",
+                    Local = "São Paulo",
+                    Lote = "2 Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+                } 
+            };
+            */ 
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        //public ActionResult<Evento> Get(int id)
+        //public IActionResult Get(int id)
+        public async Task<IActionResult> Get(int id)
+        {
+           /* return  new Evento[] { 
+                new Evento() { 
+                    EventoId = 1,
+                    Tema = "Angular e .NET Core",
+                    Local = "Belo Horizonte",
+                    Lote = "1 Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                } ,
+                new Evento() { 
+                    EventoId = 2,
+                    Tema = "Angular e Suas Novidades",
+                    Local = "São Paulo",
+                    Lote = "2 Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+                } 
+            }.
+             */
+           //return _context.Eventos.FirstOrDefault(x => x.EventoId == id);
+
+            try
+            {
+                //var results = _context.Eventos.ToList();
+                //var results = _context.Eventos.FirstOrDefault(x => x.EventoId == id);
+                var results = await _context.Eventos.FirstOrDefaultAsync(x => x.Id == id);
+                return Ok(results);       
+            }
+            catch (System.Exception)
+            {
+                
+                //return BadRequest(); 
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }           
+
+
+
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
